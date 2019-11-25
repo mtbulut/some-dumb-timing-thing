ae32a7bd18794f9c08d80fe059e8b3df2ae56854
Memet Bulut <mbulut@tamusa.edu>
2019-11-25 12:34:34 -0600

    now added

--- solution.py
+++ solution.py
@@ -1,1 +1,12 @@
 import pandas as pd
+
+df = pd.read_csv("election_data.csv")
+
+# print(df)
+
+total_num_votes = df['Voter ID'].shape
+print(total_num_votes)
+
+count_df = df.groupby("Candidate").count()
+# print(count_df)
+print(count_df['Voter ID'])
